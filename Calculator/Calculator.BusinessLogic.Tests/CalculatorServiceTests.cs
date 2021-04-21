8be6452679e18bf355e5a3f79d699642ae142be4
Pablo Amil <lordpiti@gmail.com>
2021-04-21 20:18:36 +0200

    unit tests start

---
+++ Calculator/Calculator.BusinessLogic.Tests/CalculatorServiceTests.cs
@@ -0,0 +1,72 @@
+using Calculator.BusinessLogic.Builders;
+using Calculator.BusinessLogic.Models;
+using Calculator.BusinessLogic.Services;
+using Moq;
+using System;
+using Xunit;
+
+namespace Calculator.BusinessLogic.Tests
+{
+    public class CalculatorServiceTests
+    {
+        private ICalculatorService _calculatorService;
+        private Mock<IExpression> _expressionMock;
+        private Mock<IOperationBuilder> _operationBuilderMock;
+        private Mock<ILoggerService> _writerMock;
+
+        [Fact]
+        public void Test1()
+        {
+            // Arrange
+            var expectedResult = 0.75;
+            var expectedPrint = "test";
+
+            Setup(expectedResult, expectedPrint);
+
+            // Act
+            var result = _calculatorService.getEither(0.5, 0.5);
+
+            //Assert
+            _operationBuilderMock.Verify(mock => mock.BuildBinary<EitherExpression>(0.5, 0.5), Times.Once);
+            _expressionMock.Verify(mock => mock.evaluate(), Times.Once());
+            _writerMock.Verify(mock => mock.WriteLine(It.IsAny<DateTime>(), expectedPrint, expectedResult.ToString()), Times.Once);
+
+            Assert.Equal(result, expectedResult);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            // Arrange
+            var expectedResult = 0.25;
+            var expectedPrint = "test";
+
+            Setup(expectedResult, expectedPrint);
+
+            // Act
+            var result = _calculatorService.getCombinedWith(0.5, 0.5);
+
+            //Assert
+            _operationBuilderMock.Verify(mock => mock.BuildBinary<CombinedWithExpression>(0.5, 0.5), Times.Once);
+            _expressionMock.Verify(mock => mock.evaluate(), Times.Once());
+            _writerMock.Verify(mock => mock.WriteLine(It.IsAny<DateTime>(), expectedPrint, expectedResult.ToString()), Times.Once);
+
+            Assert.Equal(result, expectedResult);
+        }
+
+        private void Setup(double expectedResult, string expectedPrint)
+        {
+            _expressionMock = new Mock<IExpression>();
+            _expressionMock.Setup(x => x.evaluate()).Returns(expectedResult);
+            _expressionMock.Setup(x => x.print()).Returns(expectedPrint);
+
+            _operationBuilderMock = new Mock<IOperationBuilder>();
+            _operationBuilderMock.Setup(x => x.BuildBinary<BinaryExpression>(It.IsAny<double>(), It.IsAny<double>())).Returns(_expressionMock.Object);
+
+            _writerMock = new Mock<ILoggerService>();
+            _writerMock.Setup(x => x.WriteLine(It.IsAny<DateTime>(), It.IsAny<string>(), It.IsAny<string>()));
+
+            _calculatorService = new CalculatorService(_operationBuilderMock.Object, _writerMock.Object);
+        }
+    }
+}
---
+++ Calculator/Calculator.BusinessLogic.Tests/ExpressionTests.cs
@@ -0,0 +1,36 @@
+﻿using Calculator.BusinessLogic.Builders;
+using Calculator.BusinessLogic.Models;
+using Calculator.BusinessLogic.Services;
+using Moq;
+using System;
+using Xunit;
+
+namespace Calculator.BusinessLogic.Tests
+{
+    public class ExpressionTests
+    {
+        [Fact]
+        public void AdditionExpression_EvaluatesCorrectly()
+        {
+            var additionExpression = new AdditionExpression(new NumberExpression(0.5), new NumberExpression(0.5));
+
+            Assert.Equal(1, additionExpression.evaluate());
+        }
+
+        [Fact]
+        public void CombinedWithExpression_EvaluatesCorrectly()
+        {
+            var combinedWithExpression = new CombinedWithExpression(new NumberExpression(0.5), new NumberExpression(0.5));
+
+            Assert.Equal(0.25, combinedWithExpression.evaluate());
+        }
+
+        [Fact]
+        public void EitherExpression_EvaluatesCorrectly()
+        {
+            var eitherExpression = new EitherExpression(new NumberExpression(0.5), new NumberExpression(0.5));
+
+            Assert.Equal(0.75, eitherExpression.evaluate());
+        }
+    }
+}
